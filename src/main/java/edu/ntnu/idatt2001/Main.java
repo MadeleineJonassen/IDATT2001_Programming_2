4a89976a28c72000338da33bee5a4eca5191abef
MaddyPaddy2001 <madelesj@stud.ntnu.no>
2023-02-21 16:09:26 +0100

    Add methods in Player, and test in Main

--- src/main/java/edu/ntnu/idatt2001/Main.java
+++ src/main/java/edu/ntnu/idatt2001/Main.java
@@ -1,7 +1,43 @@
 package edu.ntnu.idatt2001;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private final Scanner in = new Scanner(System.in);
+    private static final Player info = new Player();
+
+    public static void main(String[] args) throws IllegalAccessException {
+        Main client = new Main();
+        client.playerData();
+        client.menu();
     }
+
+    public void playerData() throws IllegalAccessException {
+        info.newPlayer("Emil super rumpe",100,100, 100, null);
+    }
+
+    public void menu(){
+        while (true){
+            String returnNewNameStatus;
+            System.out.println("Hello, Traveler! What is your name? ");
+            String name = Confirm.stringConfirm("Your name: ", "\n ~ Sorry, I didn't quite catch that." );
+            try {
+                returnNewNameStatus = info.getName(name);
+            } catch (Exception err) {
+                err.printStackTrace();
+                returnNewNameStatus = "Something happened to the application, please try again. ";
+            }
+
+            System.out.println("");
+            System.out.println("Hello, " + Player.getName(name));
+            System.out.println("Your health is " + Player.getHealth() + "hp");
+            System.out.println("You have " + Player.getGold() + " gold");
+
+            System.out.println(Player.toString());
+
+            break;
+        }
+    }
+
 }
