8b1a84e47747781ff0e6739e850c5040d13104bc
Kristiane Skogvang Kolshus <krisskol@stud.ntnu.no>
2023-02-23 13:03:08 +0100

    Improve methods for Player, Main classes

--- src/main/java/edu/ntnu/idatt2001/Main.java
+++ src/main/java/edu/ntnu/idatt2001/Main.java
@@ -5,36 +5,31 @@ import java.util.Scanner;
 public class Main {
 
     private final Scanner in = new Scanner(System.in);
-    private static final Player info = new Player();
+    private Player player = new Player("Arthur",10,0,0);
 
     public static void main(String[] args) throws IllegalAccessException {
         Main client = new Main();
-        client.playerData();
         client.menu();
     }
-
-    public void playerData() throws IllegalAccessException {
-        info.newPlayer("Emil super rumpe",100,100, 100, null);
-    }
-
+    
     public void menu(){
         while (true){
             String returnNewNameStatus;
             System.out.println("Hello, Traveler! What is your name? ");
             String name = Confirm.stringConfirm("Your name: ", "\n ~ Sorry, I didn't quite catch that." );
             try {
-                returnNewNameStatus = info.getName(name);
+                returnNewNameStatus = player.getName(name);
             } catch (Exception err) {
                 err.printStackTrace();
                 returnNewNameStatus = "Something happened to the application, please try again. ";
             }
 
             System.out.println("");
-            System.out.println("Hello, " + Player.getName(name));
-            System.out.println("Your health is " + Player.getHealth() + "hp");
-            System.out.println("You have " + Player.getGold() + " gold");
+            System.out.println("Hello, " + player.getName(name));
+            System.out.println("Your health is " + player.getHealth() + "hp");
+            System.out.println("You have " + player.getGold() + " gold");
 
-            System.out.println(Player.toString());
+            System.out.println(player.toString());
 
             break;
         }
