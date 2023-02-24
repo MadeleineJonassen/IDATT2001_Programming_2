da2f25b81b453ccad1779d9c87fd7bf9dd5878a1
Kristiane Skogvang Kolshus <krisskol@stud.ntnu.no>
2023-02-24 12:20:47 +0100

    Add unit test for Link class

--- src/test/java/edu/ntnu/idatt2001/LinkTest.java
+++ src/test/java/edu/ntnu/idatt2001/LinkTest.java
@@ -1,7 +1,10 @@
 package edu.ntnu.idatt2001;
 
-import edu.ntnu.idatt2001.Action.GoldAction;
+import edu.ntnu.idatt2001.Action.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,16 +25,36 @@ class LinkTest {
   }
   
   @Test
-  void addActionTest() {
-    testLink.addAction(new GoldAction(10));
+  void addGetActionTest() {
+    Action testAction = new GoldAction(10);
+    testLink.addAction(testAction);
+    assertEquals(testAction, testLink.getActions().get(0));
   }
   
+   @Test
+   void addGetActionsTest(){
+     Action testAction1 = new GoldAction(10);
+     Action testAction2 = new HealthAction(10);
+     Action testAction3 = new ScoreAction(50);
+     Action testAction4 = new InventoryAction("TestItem");
+     testLink.addAction(testAction1);
+     testLink.addAction(testAction2);
+     testLink.addAction(testAction3);
+     testLink.addAction(testAction4);
+     assertEquals(testAction4, testLink.getActions().get(3));
+   }
+  
   @Test
-  void testToStringTest() {
+  void toStringTest() {
+    assertEquals(testText + ", next: " + testReference, testLink.toString());
   }
   
   @Test
   void testEquals() {
+    String testText2 = "testText";
+    String testReference2 = "testReference";
+    Link testLink2 = new Link(testText2, testReference2);
+    assertTrue(testLink2.equals(testLink));
   }
   
   @Test
