3f8d3fb808783cda670e7f00e5126841dc8f6f54
MaddyPaddy2001 <madelesj@stud.ntnu.no>
2023-03-28 13:09:04 +0200

    Add FileReader to Main.class

--- src/main/java/edu/ntnu/idatt2001/Main.java
+++ src/main/java/edu/ntnu/idatt2001/Main.java
@@ -1,38 +1,32 @@
 package edu.ntnu.idatt2001;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     private final Scanner in = new Scanner(System.in);
-    private Player player = new Player("Arthur",10,0,0);
+    private Player player = new Player("Arthur", 10, 0, 0);
 
     public static void main(String[] args) throws IllegalAccessException {
         Main client = new Main();
         client.menu();
     }
-    
-    public void menu(){
-        while (true){
-            String returnNewNameStatus;
-            System.out.println("");
-            System.out.println("Hello, traveler! What is your name? ");
-            String name = Confirm.stringConfirm("Your name: ", "\n ~ Sorry, I didn't quite catch that." );
-            try {
-                returnNewNameStatus = player.getName(name);
-            } catch (Exception err) {
-                err.printStackTrace();
-                returnNewNameStatus = "Something happened to the application, please try again. ";
+
+    public void menu() {
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("C:\\NTNU\\Programmering 2\\paths\\src\\main\\resources\\Stories.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
             }
-
-            System.out.println("\nHello, " + player.getName(name));
-            System.out.println(player.toString());
-            System.out.println("-------------------------------------------------------------------------------------");
-            System.out.println("");
-            System.out.println("\t# Something happened! #\n");
-
-            break;
+            reader.close();
+        } catch (
+                IOException e) {
+            e.printStackTrace();
         }
     }
-
 }
