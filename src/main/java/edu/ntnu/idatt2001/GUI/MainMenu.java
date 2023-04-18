5a47b3f07bf948d6f9038d0d386a98fae79c038c
MaddyPaddy2001 <madelesj@stud.ntnu.no>
2023-04-18 13:48:23 +0200

    Add proper GUI for Menu

--- src/main/java/edu/ntnu/idatt2001/GUI/MainMenu.java
+++ src/main/java/edu/ntnu/idatt2001/GUI/MainMenu.java
@@ -1,15 +1,20 @@
 package edu.ntnu.idatt2001.GUI;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 
 public class MainMenu extends Application {
   Stage window;
-  BorderPane layout;
+  Scene menuScene, game, end;
 
   public static void main (String[] args) {
     launch();
@@ -18,9 +23,38 @@ public class MainMenu extends Application {
   @Override
   public void start (Stage menuStage) throws Exception {
 
+    //Start Button
+    HBox startButtonLayout = new HBox();
+      startButtonLayout.setAlignment(Pos.CENTER);
+      startButtonLayout.setPadding(new Insets(20));
+    Button startBtn = new Button("Start Game");
+      startBtn.setOnAction( e -> System.out.println("Start button pressed"));
+    startButtonLayout.getChildren().addAll(startBtn);
+
+    //Title Menu
+    VBox titleLayout = new VBox();
+      titleLayout.setPadding(new Insets(20));
+      titleLayout.setSpacing(10);
+      titleLayout.setAlignment(Pos.CENTER);
+    Label menuTitle = new Label("Paths");
+      menuTitle.setFont(Font.font(40));
+    Label underTitle = new Label("Welcome to a story based game");
+    titleLayout.getChildren().addAll(menuTitle,underTitle);
+
+    //Whole menu layout
+    BorderPane menu = new BorderPane();
+      menu.setTop(titleLayout);
+      menu.setBottom(startButtonLayout);
+
     window = menuStage;
     window.setTitle("Menu for Paths");
+    window.setResizable(false);
+    menuScene = new Scene(menu, 600, 600);
+    window.setScene(menuScene);
+    window.show();
 
+
+    /* Put above
     //Settings Menu
     Menu fileMenu = new Menu("Settings");
       //Setting items
@@ -71,18 +105,13 @@ public class MainMenu extends Application {
       CheckMenuItem autoSave = new CheckMenuItem("Enable Autosave");
       autoSave.setSelected(true);
       goals.getItems().addAll(goldDiggerGoal, healGoal, itemGoal, autoSave);
-  //May change to Radio menu, so the user only can select one item...?
+  //May change to RadioMenu, so the user only can select one item...?
 
 
     //Main menu bar
     MenuBar menuBar = new MenuBar();
     menuBar.getMenus().addAll(fileMenu, newGoal, goals);
-
-    layout = new BorderPane();
-    layout.setTop(menuBar);
-    Scene scene = new Scene(layout, 500, 400);
-    window.setScene(scene);
-    window.show();
+    */
 
   }
 }
