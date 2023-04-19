d7302f13c48124ea6bf4e087cb53ae0f01e97805
Kristiane Skogvang Kolshus <krisskol@stud.ntnu.no>
2023-04-19 12:23:43 +0200

    Refactor Passage, Link, Story classes

--- src/main/java/edu/ntnu/idatt2001/Story.java
+++ src/main/java/edu/ntnu/idatt2001/Story.java
@@ -9,6 +9,8 @@ public class Story {
   private Map<Link, Passage> passages = new HashMap<>();
   private final Passage openingPassage;
   
+  
+  //constructor, initializes a story object with title and opening passage
   public Story(String title, Passage openingPassage) {
     if(title.isEmpty()){
       throw new IllegalArgumentException("Title cannot be empty");
@@ -24,37 +26,53 @@ public class Story {
   }
   
   public Passage getOpeningPassage() {
+    //return deep copy?
     return openingPassage;
   }
   
+  //Adds a new passage to the story-list
+  //Constructs a custom Link-object to use as key in hashMap
   public void addPassage(Passage passage){
+    //if passage is not linked to? 'Unreachable' passages.
+    //get broken links, see link references, or create link, use .equals-method
+    
     if(passages.containsValue(passage)){
       throw new IllegalArgumentException("This passage had already been added to the story");
     }
     
+    
     String passageTitle = passage.getTitle();
     
+    passages.put(new Link(passageTitle, passageTitle), passage);
+    
+    /*
     for(Passage listedPassage : getPassages()){
       for(Link link : listedPassage.getLinks()){
         if(link.getReference().equals(passageTitle)){
           passages.put(link,passage);
         }
       }
-    }
+    }*/
   }
   
+  //Returns a passage, specified by the Link-object parameter
   public Passage getPassage(Link link){
     if(!passages.containsKey(link)){
       throw new IllegalArgumentException("No such link in the story.");
     }
+    //return deep copy?
+    
     return passages.get(link);
   }
   
+  //Returns a list of all the passages currently contained in the story
   public Collection<Passage> getPassages() {
     HashSet<Passage> passageHashSet = new HashSet<>(passages.size());
     for(Link link : passages.keySet()){
       passageHashSet.add(passages.get(link));
     }
+    //return deep copied list?
+    
     return passageHashSet;
   }
 }
