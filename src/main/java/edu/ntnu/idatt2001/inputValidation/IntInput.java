b6d35b56f41eebf98d4338ed1a1be2af88669fd3
MaddyPaddy2001 <madelesj@stud.ntnu.no>
2023-05-23 20:30:10 +0200

    CheckStyle for Input and FileWriter

--- src/main/java/edu/ntnu/idatt2001/inputValidation/IntInput.java
+++ src/main/java/edu/ntnu/idatt2001/inputValidation/IntInput.java
@@ -1,11 +1,21 @@
 package edu.ntnu.idatt2001.inputValidation;
 
+/**
+ * Validation of integer.
+ */
 public class IntInput {
-  public static int result(String rawInput){
+
+  /**
+   * Checks the input from the user.
+   *
+   * @param rawInput the raw input from the user
+   * @return the edited input
+   */
+  public static int result(String rawInput) {
     try {
       String editedString = rawInput.replace(" ", "");
       return Integer.parseInt(editedString);
-    } catch (NumberFormatException e){
+    } catch (NumberFormatException e) {
       throw new IllegalArgumentException("The input must be a number");
     }
   }
