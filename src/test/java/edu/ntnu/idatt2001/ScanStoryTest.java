5e69d91691e8a0664e2a3dafbe09c9af353f3357
MaddyPaddy2001 <madelesj@stud.ntnu.no>
2023-05-23 19:20:56 +0200

    CheckStyle for ScanStoryTest.java

--- src/test/java/edu/ntnu/idatt2001/ScanStoryTest.java
+++ src/test/java/edu/ntnu/idatt2001/ScanStoryTest.java
@@ -1,18 +1,17 @@
 package edu.ntnu.idatt2001;
 
-import edu.ntnu.idatt2001.model.Action.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 import edu.ntnu.idatt2001.fileHandler.ScanStory;
 import edu.ntnu.idatt2001.model.Action.GoldAction;
 import edu.ntnu.idatt2001.model.Action.InventoryAction;
 import edu.ntnu.idatt2001.model.Link;
 import edu.ntnu.idatt2001.model.Passage;
 import edu.ntnu.idatt2001.model.Story;
-import org.junit.jupiter.api.Test;
-
 import java.io.File;
 import java.io.FileNotFoundException;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class ScanStoryTest {
   
@@ -21,12 +20,14 @@ class ScanStoryTest {
     ScanStory scan = new ScanStory();
     Story scanStory = scan.scanStory(new File("src/main/resources/Stories/Stories.paths"));
     
-    Passage openingPassage = new Passage("Beginnings", "You are in a small, dimly lit room. There is a door in front of you.");
-    openingPassage.addLink(new Link("Try to open the door","Another room"));
+    Passage openingPassage = new Passage("Beginnings",
+            "You are in a small, dimly lit room. There is a door in front of you.");
+    openingPassage.addLink(new Link("Try to open the door", "Another room"));
     Story story = new Story("Haunted House", openingPassage);
-    Passage passage = new Passage("Another room", "The door opens to another room. You see a desk with a large, dusty book.");
-    passage.addLink(new Link("Open the book","The book of spells"));
-    passage.addLink(new Link("Go back","Beginnings"));
+    Passage passage = new Passage("Another room",
+            "The door opens to another room. You see a desk with a large, dusty book.");
+    passage.addLink(new Link("Open the book", "The book of spells"));
+    passage.addLink(new Link("Go back", "Beginnings"));
     story.addPassage(passage);
     
     assertEquals(story, scanStory);
@@ -35,7 +36,7 @@ class ScanStoryTest {
   }
   
   @Test
-  void scanLinkTest(){
+  void scanLinkTest() {
     Link link1 = new Link("text", "reference");
     link1.addAction(new GoldAction(10));
     link1.addAction(new InventoryAction("watch"));
